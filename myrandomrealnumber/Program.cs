5a608cb04b82068e6bfa013d35f86787315c73d7
Mincho Park <s2018s28@e-mirim.hs.kr>
2020-05-19 13:59:28 +0900

    숫자 맞추기

--- myrandomrealnumber/Program.cs
+++ myrandomrealnumber/Program.cs
@@ -10,6 +10,26 @@ namespace myrandomrealnumber
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int solution = random.Next() % 1000;
+            while(true)
+            {
+                Console.Write("숫자를 입력하세요 : ");
+                int input = int.Parse(Console.ReadLine());
+                if(solution == input)
+                {
+                    Console.WriteLine("정답입니다.\n");
+                    break;
+                }
+                else if(solution > input)
+                {
+                    Console.WriteLine(input + "보다는 큰 숫자입니다.\n");
+                }
+                else
+                {
+                    Console.WriteLine(input + "보다는 작은 숫자입니다.\n");
+                }
+            }
         }
     }
 }
